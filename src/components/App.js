919bcf5cbf62fddb54956f2d80c14cbc30e86dca
Michael Duberstein <dubermic@flutterint.com>
2022-10-02 18:59:15 -0400

    In Clip 404 Selecting a Language, displaying flags

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,17 @@
 import React from 'react'
 
 class App extends React.Component {
+  state = {language: 'english'};
   render(){
-    return <div className="ui container">App</div>;
+    return (
+      <div className="ui container">
+        <div>
+          Select a language:
+          <i className="flag us" />
+          <i className="flag nl" />
+        </div>
+      </div>
+    );
   }
 }
 
