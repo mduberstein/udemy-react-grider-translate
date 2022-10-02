7c67125041cc39e36b3f5d6b4f027e9794979bff
Michael Duberstein <dubermic@flutterint.com>
2022-10-02 19:06:14 -0400

    After Clip 404 Selecting a Language

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,20 @@ import React from 'react'
 
 class App extends React.Component {
   state = {language: 'english'};
+
+  onLanguageChange = language => {
+    this.setState({language});
+  }
+
   render(){
     return (
       <div className="ui container">
         <div>
           Select a language:
-          <i className="flag us" />
-          <i className="flag nl" />
+          <i className="flag us" onClick={() => this.onLanguageChange('english')} />
+          <i className="flag nl" onClick={() => this.onLanguageChange('dutch')} />
         </div>
+        {this.state.language}
       </div>
     );
   }
