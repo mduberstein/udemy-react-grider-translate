c1010d0a4acc6faddf97b1c99e0740c05168909d
Michael Duberstein <dubermic@flutterint.com>
2022-10-02 21:45:29 -0400

    After Clip 407 Creating Context Objects

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,20 @@
 import React from "react";
+import LanguageContext from "./contexts/LanguageContext";
 
 class Button extends React.Component {
+  // ALT 1
+  // contextType is a special property name, must ber exactly that
+  // once it is set, this.context will reference the value passed in React.createContext('english')
+  // string 'english' in this case. It can be object like {color: 'red'}, or array [1, 2, 3]
+  // like in React.createContext({color: 'red}); 
+  static contextType = LanguageContext;
+
   render() {
+    console.log("In Button->render(), this.context: ", this.context);
     return <button className="ui button primary">Submit</button>
   }
 }
 
+// ALT 2
+// Button.contextType = LanguageContext;
 export default Button;
