aeec5e2cdf8b5506953fcae8e86fccc600691749
Michael Duberstein <dubermic@flutterint.com>
2022-10-03 19:35:00 -0400

    After Clip 419 Connecting the Selector to the Store

--- src/components/LanguageSelector.js
+++ src/components/LanguageSelector.js
@@ -1,12 +1,15 @@
 import React from "react";
+import LanguageContext from './contexts/LanguageContext'
 
 class LanguageSelector extends React.Component {
+  static contextType = LanguageContext;
+
   render () {
     return (
       <div>
       Select a language:
-      <i className="flag us" onClick={() => this.props.onLanguageChange('english', 'primary')} />
-      <i className="flag nl" onClick={() => this.props.onLanguageChange('dutch', 'red')} />
+      <i className="flag us" onClick={() => this.context.onLanguageChange('english', 'primary')} />
+      <i className="flag nl" onClick={() => this.context.onLanguageChange('dutch', 'red')} />
     </div>
     );
   }
